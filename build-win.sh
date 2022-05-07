d6b77f5d174e91c5a63fe895fb5c3026f5fb5e02
yingyi <jinzhongjia@qq.com>
2022-05-07 16:29:09 +0800

    脚本微调

--- build-win.sh
+++ build-win.sh
@@ -1,1 +1,3 @@
 go build -ldflags="-s -w" -o tmp-code.exe . && upx tmp-code.exe -9
+# -H windowsgui 以下命令用于构建无需命令行启动的exe
+# go build -ldflags="-s -w -H windowsgui" -o tmp-code.exe . && upx tmp-code.exe -9
